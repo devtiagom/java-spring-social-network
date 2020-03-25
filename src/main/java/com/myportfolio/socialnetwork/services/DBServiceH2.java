e8d45e2cc78d732189366c57dbbf1e8579b997a8
Tiago Machado <tiago.o.machado@gmail.com>
2020-03-24 21:38:50 -0300

    encrypted user password

--- src/main/java/com/myportfolio/socialnetwork/services/DBServiceH2.java
+++ src/main/java/com/myportfolio/socialnetwork/services/DBServiceH2.java
@@ -4,6 +4,7 @@ import com.myportfolio.socialnetwork.domain.UserDomain;
 import com.myportfolio.socialnetwork.domain.enums.UserProfile;
 import com.myportfolio.socialnetwork.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -14,23 +15,26 @@ public class DBServiceH2 {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public void instantiateTestDatabase() {
         UserDomain user01 = new UserDomain(
                 "Fulano de Tal",
                 "fulano.tal@gmail.com",
-                "1234"
+                bCryptPasswordEncoder.encode("1234")
         ).addProfile(UserProfile.USER).addProfile(UserProfile.ADMIN);
 
         UserDomain user02 = new UserDomain(
                 "Beltrano de Tal",
                 "beltrano.tal@gmail.com",
-                "1234"
+                bCryptPasswordEncoder.encode("1234")
         ).addProfile(UserProfile.USER);
 
         UserDomain user03 = new UserDomain(
                 "Ciclano de Tal",
                 "ciclano.tal@gmail.com",
-                "1234"
+                bCryptPasswordEncoder.encode("1234")
         ).addProfile(UserProfile.USER);
 
         userRepository.saveAll(Arrays.asList(user01, user02, user03));
--- src/main/java/com/myportfolio/socialnetwork/services/UserService.java
+++ src/main/java/com/myportfolio/socialnetwork/services/UserService.java
@@ -5,6 +5,7 @@ import com.myportfolio.socialnetwork.domain.enums.UserProfile;
 import com.myportfolio.socialnetwork.dtos.UserRequestDTO;
 import com.myportfolio.socialnetwork.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,34 +16,41 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public List<UserDomain> index() {
-        return userRepository.findAll();
+        return this.userRepository.findAll();
     }
 
     public UserDomain show(Long id) {
-        return userRepository.findById(id).isPresent() ? userRepository.findById(id).get() : null;
+        return this.userRepository.findById(id).isPresent() ? this.userRepository.findById(id).get() : null;
     }
 
     public UserDomain store(UserRequestDTO userDTO) {
-        return userRepository.save(this.fromDTO(userDTO).addProfile(UserProfile.USER));
+        return this.userRepository.save(this.fromDTO(userDTO).addProfile(UserProfile.USER));
     }
 
     public void update(Long id, UserRequestDTO userDTO) {
         UserDomain user = this.show(id);
 
         if (user != null) {
-            user.setName(userDTO.getName());
-            user.setEmail(userDTO.getEmail());
-            userRepository.save(user);
+            if (userDTO.getName() != null) user.setName(userDTO.getName());
+            if (userDTO.getEmail() != null) user.setEmail(userDTO.getEmail());
+            this.userRepository.save(user);
         }
     }
 
     public void destroy(Long id) {
         UserDomain userFromDB = this.show(id);
-        if (userFromDB != null) userRepository.delete(userFromDB);
+        if (userFromDB != null) this.userRepository.delete(userFromDB);
     }
 
     private UserDomain fromDTO(UserRequestDTO userDTO) {
-        return new UserDomain(userDTO.getName(), userDTO.getEmail(), userDTO.getPassword());
+        return new UserDomain(
+                userDTO.getName(),
+                userDTO.getEmail(),
+                this.bCryptPasswordEncoder.encode(userDTO.getPassword())
+        );
     }
 }
