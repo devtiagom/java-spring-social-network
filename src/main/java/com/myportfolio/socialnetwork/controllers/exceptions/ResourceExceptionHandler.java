9d4596f2d96cc8b54d700aa00eaf03cab88562bb
Tiago Machado <tiago.o.machado@gmail.com>
2020-03-26 02:04:26 -0300

    customised validation message

--- src/main/java/com/myportfolio/socialnetwork/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/myportfolio/socialnetwork/controllers/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.myportfolio.socialnetwork.controllers.exceptions;
 import com.myportfolio.socialnetwork.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,5 +17,16 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/myportfolio/socialnetwork/controllers/exceptions/ValidationError.java
@@ -0,0 +1,22 @@
+package com.myportfolio.socialnetwork.controllers.exceptions;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    public void addError(String fielName, String message) {
+        this.errors.add(new FieldMessage(fielName, message));
+    }
+}
